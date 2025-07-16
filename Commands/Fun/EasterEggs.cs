04b78561a45b2d23978c8c00a6b898323d13677b
JustABrian <107365004+JustSomeNoname@users.noreply.github.com>
2025-07-16 21:15:21 +0200

    Update EasterEggs.cs
    Updated command logic & removed CheckBaseAttribute

--- Commands/Fun/EasterEggs.cs
+++ Commands/Fun/EasterEggs.cs
@@ -1,4 +1,4 @@
-﻿#region
+#region
 
 using AGC_Management.Attributes;
 using System;
@@ -11,47 +11,29 @@ namespace AGC_Management.Commands.Fun;
 
 public class AGCEasterEggs : BaseCommandModule
 {
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    private class RandomGuildCooldownAttribute : CheckBaseAttribute
-    {
-        private static readonly ConcurrentDictionary<ulong, DateTime> _cooldowns = new();
-        private readonly int _minSeconds;
-        private readonly int _maxSeconds;
-
-        public RandomGuildCooldownAttribute(int minSeconds, int maxSeconds)
-        {
-            _minSeconds = minSeconds;
-            _maxSeconds = maxSeconds;
-        }
-
-        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
-        {
-            var guildId = ctx.Guild?.Id ?? ctx.Channel.Id;
-
-            if (_cooldowns.TryGetValue(guildId, out var lastUsed))
-            {
-                var now = DateTime.UtcNow;
-
-                if (now < lastUsed)
-                {
-                    var remaining = (int)(lastUsed - now).TotalSeconds;
-                    _ = ctx.RespondAsync($"⏳ Savaş hatte eben erst eine Tomate, erinnere ihn gerne später erneut :3");
-                    return Task.FromResult(false);
-                }
-            }
-
-            var cooldownDuration = new Random().Next(_minSeconds, _maxSeconds + 1);
-            _cooldowns[guildId] = DateTime.UtcNow.AddSeconds(cooldownDuration);
-
-            return Task.FromResult(true);
-        }
-    }
+    private static readonly ConcurrentDictionary<ulong, DateTime> _cooldowns = new();
+    private static readonly Random _rng = new();
 
     [AGCEasterEggsEnabled]
-    [RandomGuildCooldown(60, 500)]
     [Command("savas")]
     public async Task Savas(CommandContext ctx)
     {
+        var guildId = ctx.Guild?.Id ?? ctx.Channel.Id;
+
+        if (_cooldowns.TryGetValue(guildId, out var lastUsed))
+        {
+            var now = DateTime.UtcNow;
+            if (now < lastUsed)
+            {
+                var remaining = (int)(lastUsed - now).TotalSeconds;
+                await ctx.RespondAsync($"⏳ Savaş hatte eben erst eine Tomate, erinnere ihn gerne später erneut :3");
+                return;
+            }
+        }
+
+        int cooldown = _rng.Next(60, 501);
+        _cooldowns[guildId] = DateTime.UtcNow.AddSeconds(cooldown);
+
         await ctx.Channel.SendMessageAsync("POV <@443114493992763392>:\n### Ich liebe Tomaten <3\n[￶](https://meow.justabrian.me/-fzQMJB2y4P/Ryuunosuke_Akasaka.webp)");
     }
 }
